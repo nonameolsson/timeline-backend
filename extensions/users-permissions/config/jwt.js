d763603c0a929063d58015a3529d54da90c77929
Andreas Olsson <noname.olsson@gmail.com>
2020-06-15 12:21:16 +0200

    build: add commitizen and standard-version

--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '1f946e01-27e5-42b3-93df-5d5100dacb32'
-};
+  jwtSecret: process.env.JWT_SECRET || '1f946e01-27e5-42b3-93df-5d5100dacb32',
+}
